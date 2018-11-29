0d299f328a823296228023761be86c964d8823c7
Frederick Boehm <frederick.boehm@gmail.com>
2018-11-29 10:45:18 -0600

    updated figures and new files for condor

---
+++ Rscript/keller-pvl-chr2-2018-11-29.R
@@ -0,0 +1,71 @@
+# The code below performs the two-dimensional QTL scan over the specified genomic region. 
+# We analyze the chromosome 2 hotspot from Keller et al 2018 (GENETICS).
+# Each scan considers two traits. One of those two is the Hnf4a gene expression level. 
+# Keller et al 2018 state that Hnf4a is causal for 88 of the 147 hotspot nonlocal traits.
+
+##First read in the arguments listed at the command line
+
+args <- R.utils::commandArgs(trailingOnly = TRUE, asValues = TRUE)
+print(args)
+##args is now a list of character vectors
+print(args$argname)
+proc_num <- as.numeric(args$argname)
+print(proc_num)
+(hot_indic <- proc_num %% 139 + 1) # define hot_indic 
+(local_indic <- proc_num %/% 139 + 1) # define local_indic
+run_num <- as.numeric(args$run_num)
+print(run_num)
+(nsnp <- as.numeric(args$nsnp))
+(s1 <- as.numeric(args$s1))
+
+###############
+
+
+# load expression traits
+readRDS("data/hotspot_expr_tib2_keller_chr2.rds") -> hotspot
+readRDS("data/keller2018-chr2-local-expr-2-2018-11-29.rds") -> local
+
+# load chr2 allele probabilities
+readRDS("genoprobs_chr2.rds") -> geno # genoprobs_chr2.rds is on SQUID
+
+# load kinship matrix (LOCO, ie, for chromosome 2, ie, doesn't use chr2 data)
+readRDS("data/kinship_chr2.rds") -> kinship
+
+# load covariates
+readRDS("data/addcovar.rds") -> covar
+
+
+# remove subjects with missing data
+
+id2keep <- local$mouse_id
+gg <- geno[[1]]
+gg2 <- gg[rownames(gg) %in% id2keep, , ]
+kk <- kinship[[1]]
+kk2 <- kk[rownames(kk) %in% id2keep, colnames(kk) %in% id2keep]
+cc2 <- covar[rownames(covar) %in% id2keep, ]
+
+
+# create matrix of two expression traits
+pheno <- cbind(local[ , local_indic, drop = FALSE], hotspot[ , hot_indic, drop = FALSE])
+rownames(pheno) <- local$mouse_id
+# verify that names match in all objects
+sum(rownames(pheno) == rownames(gg2))
+sum(rownames(pheno) == rownames(kk2))
+sum(rownames(pheno) == colnames(kk2))
+sum(rownames(pheno) == rownames(cc2))
+phenames <- c(colnames(local2)[local_indic], colnames(hotspot)[hot_indic])
+# two-dimensional scan
+
+library(qtl2pleio)
+s_out <- scan_pvl(probs = gg2,
+         pheno = pheno,
+         kinship = kk2,
+         covariates = cc2[ , -5], # need to remove column 5 because we have no mice from wave 5
+         start_snp1 = s1,
+         n_snp = nsnp
+           )
+
+colnames(pheno)
+fn_out <- paste0("pvl-run", run_num, "_", proc_num, "_", paste(phenames, collapse = "_"), ".txt")
+write.table(s_out, fn_out, quote = FALSE)
+q("no")
---
+++ shell_scripts/keller-pvl-chr2-2018-11-29.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# untar your R installation
+tar -xzf R-2018-09-30a.tar.gz
+tar -xzf SLIBS.tar.gz
+# make sure the script will use your R installation
+export PATH=$(pwd)/R/bin:$PATH
+export LD_LIBRARY_PATH=$(pwd)/SS:$LD_LIBRARY_PATH
+# run R, with the name of your  R script
+R CMD BATCH '--args argname='$1' nsnp='$2' s1='$3' run_num='$4'' Rscript/keller-pvl-chr2-2018-11-29.R 'pvl_run'$4'-'$1'.Rout'
+
+
+#rm *.Rout
